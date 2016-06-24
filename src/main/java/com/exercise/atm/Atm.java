67c0519aa8c2b1d04cc9075b465fc899b26c03d6
leva16 <ylevchuk@gmail.com>
2016-06-24 11:40:54 +0300

    Initial commit

---
+++ src/main/java/com/exercise/atm/Atm.java
@@ -0,0 +1,11 @@
+package com.exercise.atm;
+
+/**
+ * Created by leva on 6/23/16.
+ */
+public class Atm {
+    public static void main(String args[])
+    {
+        System.out.println("Hello!");
+    }
+}
