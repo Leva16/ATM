74ea8ecc9c86064a90d25cba01eb7edb13ede6a3
leva16 <ylevchuk@gmail.com>
2016-06-29 18:01:00 +0300

    1

--- src/main/java/com/exercise/atm/Atm.java
+++ src/main/java/com/exercise/atm/Atm.java
@@ -2,27 +2,17 @@ package com.exercise.atm;
 
 import com.exercise.atm.Dispenser;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Atm {
-    public static void main(String args[]) {
-        Dispenser atm = new Dispenser();
-        Map<Integer, Integer> map = new LinkedHashMap<Integer, Integer>();
-        map.put(20, 13);
-        map.put(50, 9);
-        map.put(100, 15);
-        map.put(200, 11);
-        map.put(500, 7);
-        atm.setDispenser(map);
-        //System.out.println(atm.atmSum());
-        ReadPropertiesFile readPropertiesFile = new ReadPropertiesFile();
-        try {
-            System.out.println(readPropertiesFile.getPropValues());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static void main(String args[]) throws Exception {
 
+
+        List<Notes> notes = new ArrayList<Notes>();
+
+        Dispenser atm = new Dispenser();
+
+        System.out.println(atm.atmSum());
 
     }
 }
--- src/main/java/com/exercise/atm/Dispenser.java
+++ src/main/java/com/exercise/atm/Dispenser.java
@@ -1,15 +1,29 @@
 package com.exercise.atm;
 
 import java.util.*;
+import java.io.*;
+import java.lang.Exception;
 
 public class Dispenser {
+
+    ReadPropertiesFile readPropertiesFile = new ReadPropertiesFile();
+    private Map<Integer, Integer> cash = new TreeMap();
+    public void setCash() throws Exception{
+        this.cash = readPropertiesFile.getPropValues();
+    }
+
+    private List<Notes> notes = new ArrayList<Notes>();
+
+    public void setNotes(List<Notes> notes) throws Exception{
+        setCash();
+        for (Map.Entry<Integer, Integer> entry : cash.entrySet()) {
+            notes.add(new Notes(entry.getKey(), entry.getValue()));
+        }
+
+        this.notes = notes;
+    }
+
     private Map<Integer, Integer> dispenser = new LinkedHashMap<Integer, Integer>();
-
-    public static List<Integer> list = new ArrayList<Integer>();
-    Notes A20 = new Notes(20, 13);
-
-
-
     public Map<Integer, Integer> getDispenser() {
         return dispenser;
     }
@@ -23,10 +37,11 @@ public class Dispenser {
      * @atmSum
      * returm amount of money in ATM.
      */
-    public int atmSum() {
+    public int atmSum() throws Exception {
+        Map<Integer, Integer> money = readPropertiesFile.getPropValues();
         int sum = 0;
-        for (Map.Entry entry : getDispenser().entrySet()) {
-            sum += ((Integer) entry.getKey() * (Integer) entry.getValue());
+        for (Map.Entry<Integer, Integer> entry : money.entrySet()) {
+            sum += (entry.getKey() * entry.getValue());
         }
         return sum;
     }
